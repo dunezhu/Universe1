7f7be9ee8f0222abc542b86b59458f5e3b2511f6
Jun Zhu <123275291+dunezhu@users.noreply.github.com>
2023-06-19 08:34:07 -0700

    Initial Commit

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 100;
+
+    Transform myT;
+
+    private void Awake()
+    {
+        myT = transform;
+    }
+    private void Update()
+    {
+        Thrust();
+    }
+
+    void Thrust()
+    {
+        myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+    }
+}
